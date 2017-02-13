da6a784fdeaab9a580adb9484f5aac85bc703c51
ntonjeta <ntonjeta@autistici.org>
2017-02-13 15:10:28 +0100

    add license

--- include/Core/EvolutionContext.h
+++ include/Core/EvolutionContext.h
@@ -1,3 +1,23 @@
+/* * Bellerophon - Design Space Exploration for Approximate Computing technique using
+ * Gentic Algorithm
+ * Copyright (C) 2017 Antonio Tammaro <ntonjeta@autistici.org>
+ *
+ * This file is part of Bellerophon.
+ *
+ * Bellerophon is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Bellerophon is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Bellerophon.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 /******************************************************************************
  * @file   EvolutionContext.h
  * @author Antonio Tammaro
